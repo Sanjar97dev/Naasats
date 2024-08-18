751a853da4db6c09d70652c2d27074c84a8ef9a4
macbookpro 2022 <macbookpro2016@Macbookpro.local>
2024-08-18 23:14:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Naasats from './components/naasat/Naasats'
+
+const App = () => {
+  return (
+    <div>
+      <Naasats/>
+    </div>
+  )
+}
+
+export default App
